b76085255634aa53178cdb36f89a7ec100068f90
fouad el fakhori <58748839+fFakhori@users.noreply.github.com>
2021-02-24 12:54:35 +0100

    Upadte

--- src/com/servelt/Login.java
+++ src/com/servelt/Login.java
@@ -15,8 +15,7 @@ import com.DAO.UserDAO;
 import com.javaBeans.HomeData;
 import com.javaBeans.User;
 
-
-@WebServlet("/Login")
+@WebServlet({"/Login", "/"})
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
